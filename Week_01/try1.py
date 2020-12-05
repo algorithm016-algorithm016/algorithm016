814e7482fff99b588cd2fea0cf83520ba495d634
bookwormai <david.yi.li.april@gmail.com>
2020-12-04 20:09:19 -0500

    new change

--- Week_01/try1.py
+++ Week_01/try1.py
@@ -5,6 +5,9 @@ Created on Sun Sep  6 13:38:53 2020
 @author: David Li
 """
 
-print("I love github");
+def g():
+    dict1 = {1: 1, 2: 2}
+    return dict1
+print f"the dict is {dict1}"
 print("i love life");
 print("i love work")
